5ac02de74582418f8c3d589f3e363cdc6394274e
erez-xps-wsl <erezgiv.work@gmail.com>
2025-01-01 14:25:51 +0200

    fix formatting

--- js/phone_number.js
+++ js/phone_number.js
@@ -1,7 +1,8 @@
-function createPhoneNumber(numbers){
-    part1 = numbers.slice(0,3).join('')
-    part2 = numbers.slice(3,6).join('')
-    part3 = numbers.slice(6,10).join('')
+
+function createPhoneNumber(numbers) {
+    part1 = numbers.slice(0, 3).join('')
+    part2 = numbers.slice(3, 6).join('')
+    part3 = numbers.slice(6, 10).join('')
     return `(${part1}) ${part2}-${part3}`
 }
 
--- python/num_divisors.py
+++ python/num_divisors.py
@@ -1,5 +1,3 @@
-
-
 def get_divisors_number(n):
     last_div = -1
     divs_num = 0
@@ -10,12 +8,13 @@ def get_divisors_number(n):
             last_div = div
             if div != i:
                 divs_num += 1
-        if last_div > 0 and i+1 >= last_div:
+        if last_div > 0 and i + 1 >= last_div:
             break
     return divs_num
-            
+
+
 def get_divisors_number2(n):
-    divs = set();
+    divs = set()
     for i in range(1, n + 1):
         if n % i != 0:
             continue
@@ -26,6 +25,7 @@ def get_divisors_number2(n):
         else:
             divs.add(comp_div)
     return len(divs)
+
 
 def proc_arrInt(numbers):
     prime_nums = 0
@@ -42,5 +42,6 @@ def proc_arrInt(numbers):
             max_div_nums.append(n)
     max_div_nums.sort()
     return [len(numbers), prime_nums, [max_divs, max_div_nums]]
+
 
 print(proc_arrInt([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
